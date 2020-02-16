8615be0cdd2f942b2a7e85bd103ec9a6631d16f8
sleep <sleep@kaituocn.com>
2020-02-16 22:57:16 +0800

    修改topphp-swoole配置命名空间

--- config/topphpServer.php
+++ config/topphpServer.php
@@ -6,9 +6,9 @@
  * Author: sleep <sleep@kaituocn.com>
  */
 
-use topphp\swoole\server\HttpServer;
-use topphp\swoole\server\TcpServer;
-use topphp\swoole\server\WebSocketServer;
+use Topphp\TopphpSwoole\server\RpcServer;
+use Topphp\TopphpSwoole\server\HttpServer;
+use Topphp\TopphpSwoole\server\WebSocketServer;
 
 return [
     'mode'    => SWOOLE_PROCESS,                  // 运行模式 默认为SWOOLE_PROCESS
@@ -20,32 +20,25 @@ return [
             'port'      => 9501,                            // 监听端口
             'sock_type' => SWOOLE_SOCK_TCP,
             'options'   => [
-                'open_http_protocol'      => true,
+                // 开启websocket服务时设为true
                 'open_websocket_protocol' => true
             ]
         ],
-//        [
-//            'type'      => TcpServer::class,
-//            'name'      => 'top-server2',
-//            'host'      => env('SWOOLE_HOST', '127.0.0.1'), // 监听地址
-//            'port'      => 9502,                            // 监听端口
-//            'sock_type' => SWOOLE_SOCK_TCP,
-//            'options'   => [
-//                'open_http_protocol'      => true,
-//                'open_websocket_protocol' => true
-//            ]
-//        ],
-
+        [
+            'type'      => RpcServer::class,
+            'name'      => 'top-server2',
+            'host'      => env('SWOOLE_HOST', '127.0.0.1'), // 监听地址
+            'port'      => 9502,                            // 监听端口
+            'sock_type' => SWOOLE_SOCK_TCP,
+            'options'   => []
+        ],
         [
             'type'      => WebSocketServer::class,
             'name'      => 'top-server3',
             'host'      => env('SWOOLE_HOST', '127.0.0.1'), // 监听地址
             'port'      => 9503,                            // 监听端口
             'sock_type' => SWOOLE_SOCK_TCP,
-            'options'   => [
-                'open_http_protocol'      => true,
-                'open_websocket_protocol' => true
-            ]
+            'options'   => []
         ],
     ],
     'options' => [
@@ -65,5 +58,7 @@ return [
         'socket_buffer_size'    => 128 * 1024 * 1024,
         'max_request'           => 3000,
         'send_yield'            => true,
+        // 设置异步重启开关。设置为true时，将启用异步安全重启特性，Worker进程会等待异步事件完成后再退出。
+        'reload_async'          => true,
     ],
 ];
