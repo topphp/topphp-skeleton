d859237a393102cebe5b5351d3456bd65a17448b
sleep <sleep@kaituocn.com>
2020-03-06 14:05:09 +0800

    event事件机制结合swoole

--- app/subscribe/BaseServer.php
+++ app/subscribe/BaseServer.php
@@ -8,9 +8,35 @@ use Topphp\TopphpSwoole\server\TopServerEvent;
 
 class BaseServer
 {
+    /**
+     * @param array $args =[
+     *      'server' => $server,
+     *      'taskId' => $taskId,
+     *      'fromId' => $fromId,
+     *      'data'   => $data
+     * ]
+     */
+    public function onTask($args)
+    {
+        // 任务处理完成后需要调用finish方法
+        // $args['server']->finish();
+    }
+
+    /**
+     * @param array $args = [
+     *      'server' => $server,
+     *      'taskId' => $taskId,
+     *      'data'   => $data
+     * ]
+     */
+    public function onFinish($args)
+    {
+    }
+
     public function subscribe(Event $event)
     {
-//        $event->listen(TopServerEvent::ON_TASK, [$this, '']);
+        $event->listen(TopServerEvent::ON_TASK, [$this, 'onTask']);
+        $event->listen(TopServerEvent::ON_FINISH, [$this, 'onFinish']);
 //        $event->listen(TopServerEvent::ON_CLOSE, [$this, '']);
 //        $event->listen(TopServerEvent::ON_PIPE_MESSAGE, [$this, '']);
 //        $event->listen(TopServerEvent::ON_WORKER_STOP, [$this, '']);
