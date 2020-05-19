bbc92dbffd6f9ebc32b83e3dfc99bf3a5458a9a6
baiwenbiao <344147805@qq.com>
2020-05-19 23:45:19 +0800

    修改全局验证器白名单大小写说明

--- app/common/enumerate/CheckConfigEnum.php
+++ app/common/enumerate/CheckConfigEnum.php
@@ -23,14 +23,14 @@ class CheckConfigEnum
         ]
     ];
 
-    // 全局验证器Check白名单【全小写】（不参与验证的--应用名/控制器名/操作名，支持通配符设置全控制器不使用验证器--应用名/控制器名/*）
+    // 全局验证器Check白名单（不参与验证的--应用名/控制器名/操作名，支持通配符设置全控制器不使用验证器--应用名/控制器名/*）
     const CHECK_WHITE_LIST = [
 
-        // 注意：如果是多层级或多版本控制器，请务必按照规范填写全控制器，如 api/v1.index/index 或 index/layered.index/index，否则白名单将不会生效，以下为示例
+        // 注意：如果是多层级或多版本控制器，请务必按照规范填写全控制器，如 api/v1.Index/index 或 index/layered.Index/index，否则白名单将不会生效，以下为示例
 
         //--- api 应用 ---//
-        "api/v1.index/index", // index
-        "api/v2.index/index", // index
+        "api/v1.Index/index", // index
+        "api/v2.Index/index", // index
 
 
     ];
